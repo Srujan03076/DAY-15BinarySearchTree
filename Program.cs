481e9ea2fed35ef63ba89e92fdc326a570081859
“Srujan03076” <srujanmahaja5@gmail.com>
2021-10-23 12:15:25 +0530

    Add Welcome Message

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DAY_15_BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to Binary Search Tree!");
+            BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
+            binarySearch.Insert(30);
+            binarySearch.Insert(70);
+            binarySearch.Display();
+            Console.ReadLine();
+
+        }
+    }
+}
