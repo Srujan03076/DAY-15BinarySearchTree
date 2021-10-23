adba554827a21cf8713e17c8fb25c70a60f81498
“Srujan03076” <srujanmahaja5@gmail.com>
2021-10-23 12:21:54 +0530

    Create Binary Search Tree

--- Program.cs
+++ Program.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-namespace BinarySearchTree
+namespace DAY_15_BinarySearchTree
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To Binary Search Tree");
+
+            Console.WriteLine("Welcome to Binary Search Tree!");
+            BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
+            binarySearch.Insert(30);
+            binarySearch.Insert(70);
+            binarySearch.Display();
+            Console.ReadKey();
+
         }
     }
 }
